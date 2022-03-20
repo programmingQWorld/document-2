f4c7333225bfb6b570395108d2e2d24453d00639
programmingQWorld <aawoshilcq@163.com>
2022-03-20 14:25:37 +0800

    jvm

--- 一个线程 OOM 后，其他线程还能运行吗？.java
+++ 一个线程 OOM 后，其他线程还能运行吗？.java
@@ -20,10 +20,12 @@ OOM在《Java虚拟机规范》里，除'程序计数器'，虚拟机内存的'�
     -XX:+HeapDumpOnOutOf-MemoryError: 可以让虚拟机'在出现内存溢出异常的时候' 'Dump出当前的内存堆转储快照'。
 
     案例1
+
+        [代码]
         public class Heap00m {
             static class 00MObject {}
             public static void main(String[] args) {
-                List<0oMObject> list = new ArrayList<>(）；
+                List<OomObject> list = new ArrayList<>(）；
                 while(true) {
                     list.add(new 0oMobject());
                 }
@@ -38,7 +40,11 @@ OOM在《Java虚拟机规范》里，除'程序计数器'，虚拟机内存的'�
         那既然发生了，'如何解决这个内存区域的异常呢'？ 
             
             一般先通过内存映像分析工具（如'jprofile'）'对Dump'出来的'堆转储快照'进行'分析'。
+            
             第一步,分清楚到底是'内存泄漏'（Memory Leak）,还是'内存溢出'（Memory Overflow）
+            (lincq: 原则上，查看实例对象是否需要被回收，本应该被回收而实际没有回收属于内存泄漏)
+            
+            
             下图是使用 'jprofile打开的堆转储快照文件'（java_pid44526.hprof）
 
                 ['类'] [分配] [最大对象] [引用] [时间] [检查] [图表]         
@@ -59,7 +65,7 @@ OOM在《Java虚拟机规范》里，除'程序计数器'，虚拟机内存的'�
 
         '-Xms5m -Xmx10m -XX:+HeapDumpOnOutOfMemoryError'
 
-        代码：
+        代码：[在无限循环中，对集合对象做'数据存入操作']
             public class JvmThread {
                 public static void main(String[] args) throws InterruptedException {
                     Thread.sleep(15000);
@@ -182,7 +188,7 @@ OOM在《Java虚拟机规范》里，除'程序计数器'，虚拟机内存的'�
 
 
 
-    [实验二]定义大量局部变量，增大此方法帧中本地变量表的长度
+    ['实验二']定义大量局部变量，增大此方法帧中本地变量表的长度
 
         代码示例
         /**
@@ -276,150 +282,154 @@ OOM在《Java虚拟机规范》里，除'程序计数器'，虚拟机内存的'�
 
 3 '方法区和运行时常量池溢出'
 
-运行时'常量池是方法区的一部分'，所以这两个区域的溢出测试可以放到一起。
+    运行时'常量池是方法区的一部分'，所以这两个区域的溢出测试可以放到一起。
 
-HotSpot从JDK 7开始逐步“去永久代”，在JDK 8中'完全使用元空间代替永久代'，那么'方法区使用“永久代”还是“元空间”来实现，对程序有何影响呢'。
+    HotSpot从JDK 7开始逐步“去永久代”，在JDK 8中'完全使用元空间代替永久代'，那么'方法区使用“永久代”还是“元空间”来实现，对程序有何影响呢'。
 
-'String::intern()是一个本地方法'：若字符串常量池中已经包含一个等于此String对象的字符串，则返回代表池中这个字符串的String对象的引用；否则，会将此String对象包含的字符串添加到常量池，并且返回此String对象的引用。
+    'String::intern()是一个本地方法'：若字符串常量池中已经包含一个等于此String对象的字符串，则返回代表池中这个字符串的String对象的引用；否则，会将此String对象包含的字符串添加到常量池，并且返回此String对象的引用。
 
-'在JDK6或之前'HotSpot虚拟机，'常量池'都是'分配在永久代'，可以通过如下两个参数："PermSize","MaxPermSize"限制永久代的大小，即可'间接限制'其中'常量池的容量'，
+    '在JDK6或之前'HotSpot虚拟机，'常量池'都是'分配在永久代'，可以通过如下两个参数："PermSize","MaxPermSize"限制永久代的大小，即可'间接限制'其中'常量池的容量'，
 
-[代码实例] 常量池溢出测试
-/**
- * @author hubing
- * 证明字符串常量池是放在堆里面的。
- */
-public class RuntimeConstantPoo100M {
+    [代码实例] 常量池溢出测试
+    /**
+    * @author hubing
+    * 证明字符串常量池是放在堆里面的。
+    */
+    public class RuntimeConstantPoo100M {
 
-    public static void main(String[] args) {
-        // 使用集合保持着常量池引用，避免Ful1 GC回收常量池行为）
-        ArrayList<String> list = new ArrayList<>();
-        short i = 0;
-        while(true) {
-            // 在short范围内足以让6MB的PermSize产生00M了
-            list.add(String.valueOf(i ++)'.intern()');
-        }
-    }
-}
-
-结果
-Exception in thread "main" java.lang.'OutOfMemoryError': 'PermGen space '
-	at java.lang.String.intern(Native Method) 
-	at org.fenixsoft.oom.RuntimeConstantPoolOOM.main(RuntimeConstantPoolOOM.java: 18)
-
-
-可见，运行时'常量池溢出'时，在OutOfMemoryError异常后面跟随的'提示信息是“PermGen space”'，说明运行时'常量池'的确'是属于方法区的一部分'。
-
-而'使用JDK 7'或'更高版本的JDK'来'运行这段程序'并'不会得到相同的结果'，
-
-'无论是'在JDK 7中继续使 用-XX：MaxPermSize参数,'或者在'JDK 8及以上版本使用-XX：MaxMeta-spaceSize参数'把方法区容量同样限制在6MB'，
-也都不会重现JDK 6中的溢出异常，'循环将一直进行下去，永不停歇'。 
-这种变化是因为自JDK 7起，原本存'放在永久代的字符串常量池'被'移至Java堆'，
-所以在JDK 7及以上版 本，'限制方法区的容量'对 '该测试用例' 来说是'毫无意义'。
-
-那怎么办呢，如何在Java7以上版本上测试呢？这时候'使用-Xmx参数' '限制最大堆 = 6MB'就能看到以下'两种运行结果'之一，具体取决于'哪里的对象分配时' '产生了溢出'：
-
-OOM异常一： Exception in thread "main" java.lang.OutOfMemoryError: 'Java heap space '
-    at java.base/java.lang.Integer.toString(Integer.java:440) 
-    at java.base/java.lang.String.valueOf(String.java:3058) 
-    at RuntimeConstantPoolOOM.main(RuntimeConstantPoolOOM.java:12) 
-
-OOM异常二： Exception in thread "main" java.lang.OutOfMemoryError: 'Java heap space at java.base/java.util.HashMap.resize(HashMap.java:699) '
-    at java.base/java.util.HashMap.putVal(HashMap.java:658) 
-    at java.base/java.util.HashMap.put(HashMap.java:607) 
-    at java.base/java.util.HashSet.add(HashSet.java:220) 
-    at RuntimeConstantPoolOOM.main(RuntimeConstantPoolOOM.java from InputFile-Object:14)
-
-
-字符串常量池的实现位置还有很多趣事：
-    [代码示例]
-    public class RuntimeConstantPoolOOM2 {
         public static void main(String[] args) {
-            String str1 = new StringBuilder("计算机"）.append("软件").tostring();
-            System. out.println(str1.intern() == str1)；
-            String str2 = new StringBuilder("ja"）.append("va").tostring();
-            System. out.println(str2.intern() == str2)；
-        }
-    }
-
-    JDK 6中运行，结果是'两个false'
-    JDK 7中运行，一个true和一个false
-
-
-    因为JDK6的intern()会'把首次遇到的字符串实例' '复制到永久代的字符串常量池'中，返回的也是永久代里这个字符串实例的引用，
-    而由'StringBuilder创建的字符串对象实例' '在 Java 堆'，所以不可能是同一个引用，结果将返回false。
-
-    'JDK 7及以后的intern()' '无需再拷贝字符串的实例' 到永久代，'字符串常量池' '已移到Java堆'，只需'在常量池里记录'一下'首次出现的实例引用'，
-    因此'intern()返回的引用'和'由StringBuilder创建的那个字符串实例' '是同一个'。
-
-    str2比较返回false，这是因为'“java”这个字符串'在执行String-Builder.toString()之前就已经出现过了，
-    '字符串常量池中已经有它的引用'，不符合intern()方法要求“首次遇到”的原则，而“计算机软件”这个字符串则是首次出现的，因此结果返回true！
-
-
-对于'方法区的OOM测试'，'基本的思路'是'运行时产生大量类' '去填满方法区'，'直到溢出'。
-虽然'直接使用Java SE API' '也可动态产生类'（如反射时的 GeneratedConstructorAccessor和动态代理），'但操作麻烦'。 
-借助了'CGLib直接操作字节码' '运行时动态生成大量类'。 当前的很多主流框架，
-如Spring、Hibernate对类进行增强时，都会使用到 CGLib字节码增强，
-
-'当增强的类越多'，就'需要越大的方法区'以'保证动态生成的新类型可以载入内存'。 
-很多'运行于JVM的动态语言'（例如Groovy）通常'都会持续创建新类型' '来支撑语言的动态性'，
-随着这类动态语言的流行，与如下代码相似的溢出场景'也越来越容易遇到'
-
-在JDK 7中的运行结果：
-
-Caused by: java.lang.OutOfMemoryError: 'PermGen space '
-	at java.lang.ClassLoader.defineClass1(Native Method) 
-	at java.lang.ClassLoader.defineClassCond(ClassLoader.java:632) 
-	at java.lang.ClassLoader.defineClass(ClassLoader.java:616)
-JDK8及以后：可以使用
-    -XX:'MetaspaceSize'=10M
-    -XX:'MaxMetaspaceSize'=10M
-
-设置'元空间初始大小'以及'最大可分配大小'。 
-1.'如果不指定'元空间的大小，'默认情况'下，'元空间最大的大小'是'系统内存的大小'，元空间一直扩大，'虚拟机可能会消耗完' '所有的可用系统内存'。 
-2.如果'元空间内存不够用'，就'会报OOM'。 
-3.默认情况下，对应一个'64位的服务端JVM来说'，其默认的-XX:MetaspaceSize值为21MB，这就是'初始的高水位线'，一旦'元空间的大小触及这个高水位线'，就'会触发Full GC'并会'卸载没有用的类'，然后'高水位线的值'将'会被重置'。 
-4.从第3点可以知道，如果'初始化的高水位线'设置'过低'，'会频繁的触发Full GC'，高水位线会被多次调整。所以'为了避免频繁GC'以及调整高水位线，建议将-XX:MetaspaceSize'设置为较高的值'，而-XX:MaxMetaspaceSize'不进行设置'。
-
-
-'一个类如果要被gc'，要达成的条件比较苛刻。在经常'运行时生成大量动态类'的'场景'，就'需要特别关注' '这些类的回收状况'。
- 这类场景除了之前提到的程序使用了CGLib字节码增强和动态语言外，常见的还有：
-
-    '大量JSP'或动态产生JSP 文件的应用（JSP第一次运行时需要编译为Java类）
-    '基于OSGi的应用'（即使是同一个类文件，'被不同的加载器加载'也会'视为不同的类'）
-
-JDK8后，永久代完全废弃，而使用元空间作为其替代者。'在默认设置下'，前面列举的那些正常的'动态创建新类型的测试用例'已经'很难再迫使虚拟机产生方法区OOM'。 
-为了让使用者有'预防' '实际应用里' '出现类似于如上代码那样的破坏性操作'，HotSpot还是'提供了一些参数' 作为 '元空间的防御措施'：
-
-    -XX:MetaspaceSize 指定元空间的'初始空间大小'，'以字节为单位'，'达到该值'就'会触发垃圾收集'进行'类型卸载'，'同时'收集器会'对该值进行调整'。
-    如果释放了大量的空间，就适当降低该值，如果释放了很少空间，则在不超过-XX:MaxMetaspaceSize（如果设置了的话）的情况下，适当提高该值
-
-    -XX:MaxMetaspaceSize '设置元空间最大值'，'默认'-1，即'不限制'，或者说'只受限于本地内存的大小'
-    -XX:MinMetaspaceFreeRatio 在GC后控制最小的元空间剩余容量的百分比，可'减小'因为元空间不足导致的'GC频率'
-    -XX:Max-MetaspaceFreeRatio 控制最大的元空间剩余容量的百分比
-
-
-'本机直接内存溢出'
-    直接内存（Direct Memory）的容量大小可通过-XX:MaxDirectMemorySize指定，若不指定，则'默认与Java堆最大值'（-Xmx）一致。
-
-    这里越过DirectByteBuffer类，直接'通过反射获取Unsafe实例'进行'内存分配'。 
-    Unsafe类的getUnsafe()指定'只有引导类加载器'才会'返回实例'，体现了'设计者希望只有虚拟机标准类库里面的类才能使用Unsafe'，
-    'JDK10时' '才将Unsafe的部分功能' 通过VarHandle开放给外部。 因为虽然使用DirectByteBuffer分配内存'也会抛OOM'，但'它抛异常时' '并未真正向os申请分配内存'，
-    而是'通过计算得知内存无法分配'，就'在代码里手动抛了OOM'，真正申请分配内存的方法是'Unsafe::allocateMemory()'
-
-    使用unsafe分配本机内存
-        public class DirectMemoryOOM {
-            private static final int SIZE = 1024 * 1024
-            public static void main(String [〕 args) throws Exception {
-                Field unsafeField = Unsafe.class.getDeclaredFields()[0];
-                unsafeField. setAccessible(true);
-                Unsafe unsafe = (Unsafe) unsafeField.get (null);
-                while (true) {
-                    unsafe.allocateMemory(SIZE);
-                }
+            // 使用集合保持着常量池引用，避免Ful1 GC回收常量池行为）
+            ArrayList<String> list = new ArrayList<>();
+            short i = 0;
+            while(true) {
+                // 在short范围内足以让6MB的PermSize产生00M了
+                list.add(String.valueOf(i ++)'.intern()');
             }
         }
-    结果
+    }
 
-    由'直接内存导致的内存溢出'，一个'明显的特征'是'在Heap Dump文件中不会看见有什么明显异常'，
-    若发现'内存溢出之后' '产生的Dump文件很小'，'而程序中又直接或间接使用了 DirectMemory'（比如使用'NIO'），则'该考虑直接内存'了。
+    结果
+    Exception in thread "main" java.lang.'OutOfMemoryError': 'PermGen space '
+        at java.lang.String.intern(Native Method) 
+        at org.fenixsoft.oom.RuntimeConstantPoolOOM.main(RuntimeConstantPoolOOM.java: 18)
+
+
+    可见，运行时'常量池溢出'时，在OutOfMemoryError异常后面跟随的'提示信息是“PermGen space”'，说明运行时'常量池'的确'是属于方法区的一部分'。
+
+    而'使用JDK 7'或'更高版本的JDK'来'运行这段程序'并'不会得到相同的结果'，
+
+    '无论是'在JDK 7中继续使 用-XX：MaxPermSize参数,'或者在'JDK 8及以上版本使用-XX：MaxMeta-spaceSize参数'把方法区容量同样限制在6MB'，
+    也都不会重现JDK 6中的溢出异常，'循环将一直进行下去，永不停歇'。 
+    这种变化是因为自JDK 7起，原本存'放在永久代的字符串常量池'被'移至Java堆'，
+    所以在JDK 7及以上版 本，'限制方法区的容量'对 '该测试用例' 来说是'毫无意义'。
+
+    那怎么办呢，如何在Java7以上版本上测试呢？这时候'使用-Xmx参数' '限制最大堆 = 6MB'就能看到以下'两种运行结果'之一，具体取决于'哪里的对象分配时' '产生了溢出'：
+
+    OOM异常一： Exception in thread "main" java.lang.OutOfMemoryError: 'Java heap space '
+        at java.base/java.lang.Integer.toString(Integer.java:440) 
+        at java.base/java.lang.String.valueOf(String.java:3058) 
+        at RuntimeConstantPoolOOM.main(RuntimeConstantPoolOOM.java:12) 
+
+    OOM异常二： Exception in thread "main" java.lang.OutOfMemoryError: 'Java heap space at java.base/java.util.HashMap.resize(HashMap.java:699) '
+        at java.base/java.util.HashMap.putVal(HashMap.java:658) 
+        at java.base/java.util.HashMap.put(HashMap.java:607) 
+        at java.base/java.util.HashSet.add(HashSet.java:220) 
+        at RuntimeConstantPoolOOM.main(RuntimeConstantPoolOOM.java from InputFile-Object:14)
+
+
+    字符串常量池的实现位置还有很多趣事：
+        [代码示例]
+        public class RuntimeConstantPoolOOM2 {
+            public static void main(String[] args) {
+                String str1 = new StringBuilder("计算机"）.append("软件").tostring();
+                System. out.println(str1.intern() == str1)；
+                String str2 = new StringBuilder("ja"）.append("va").tostring();
+                System. out.println(str2.intern() == str2)；
+            }
+        }
+
+        JDK 6中运行，结果是'两个false'
+        JDK 7中运行，一个true和一个false
+
+
+        因为JDK6的intern()会'把首次遇到的字符串实例' '复制到永久代的字符串常量池'中，返回的也是永久代里这个字符串实例的引用，
+        而由'StringBuilder创建的字符串对象实例' '在 Java 堆'，所以不可能是同一个引用，结果将返回false。
+
+        'JDK 7及以后的intern()' '无需再拷贝字符串的实例' 到永久代，'字符串常量池' '已移到Java堆'，只需'在常量池里记录'一下'首次出现的实例引用'，
+        因此'intern()返回的引用'和'由StringBuilder创建的那个字符串实例' '是同一个'。
+
+        str2比较返回false，这是因为'“java”这个字符串'在执行String-Builder.toString()之前就已经出现过了，
+        '字符串常量池中已经有它的引用'，不符合intern()方法要求“首次遇到”的原则，而“计算机软件”这个字符串则是首次出现的，因此结果返回true！
+
+
+    对于'方法区的OOM测试'，'基本的思路'是'运行时产生大量类' '去填满方法区'，'直到溢出'。
+    虽然'直接使用Java SE API' '也可动态产生类'（如反射时的 GeneratedConstructorAccessor和动态代理），'但操作麻烦'。 
+    借助了'CGLib直接操作字节码' '运行时动态生成大量类'。 当前的很多主流框架，
+    如Spring、Hibernate对类进行增强时，都会使用到 CGLib字节码增强，
+
+    '当增强的类越多'，就'需要越大的方法区'以'保证动态生成的新类型可以载入内存'。 
+    很多'运行于JVM的动态语言'（例如Groovy）通常'都会持续创建新类型' '来支撑语言的动态性'，
+    随着这类动态语言的流行，与如下代码相似的溢出场景'也越来越容易遇到'
+
+    在JDK 7中的运行结果：
+
+    Caused by: java.lang.OutOfMemoryError: 'PermGen space '
+        at java.lang.ClassLoader.defineClass1(Native Method) 
+        at java.lang.ClassLoader.defineClassCond(ClassLoader.java:632) 
+        at java.lang.ClassLoader.defineClass(ClassLoader.java:616)
+    JDK8及以后：可以使用
+        -XX:'MetaspaceSize'=10M
+        -XX:'MaxMetaspaceSize'=10M
+
+    设置'元空间初始大小'以及'最大可分配大小'。 
+    1.'如果不指定'元空间的大小，'默认情况'下，'元空间最大的大小'是'系统内存的大小'，元空间一直扩大，'虚拟机可能会消耗完' '所有的可用系统内存'。 
+    2.如果'元空间内存不够用'，就'会报OOM'。 
+    3.默认情况下，对应一个'64位的服务端JVM来说'，其默认的-XX:MetaspaceSize值为21MB，这就是'初始的高水位线'，一旦'元空间的大小触及这个高水位线'，就'会触发Full GC'并会'卸载没有用的类'，然后'高水位线的值'将'会被重置'。 
+    4.从第3点可以知道，如果'初始化的高水位线'设置'过低'，'会频繁的触发Full GC'，高水位线会被多次调整。所以'为了避免频繁GC'以及调整高水位线，建议将-XX:MetaspaceSize'设置为较高的值'，而-XX:MaxMetaspaceSize'不进行设置'。
+
+
+    '一个类如果要被gc'，要达成的条件比较苛刻。在经常'运行时生成大量动态类'的'场景'，就'需要特别关注' '这些类的回收状况'。
+    这类场景除了之前提到的程序使用了CGLib字节码增强和动态语言外，常见的还有：
+
+        '大量JSP'或动态产生JSP 文件的应用（JSP第一次运行时需要编译为Java类）
+        '基于OSGi的应用'（即使是同一个类文件，'被不同的加载器加载'也会'视为不同的类'）
+
+    JDK8后，永久代完全废弃，而使用元空间作为其替代者。'在默认设置下'，前面列举的那些正常的'动态创建新类型的测试用例'已经'很难再迫使虚拟机产生方法区OOM'。 
+    为了让使用者有'预防' '实际应用里' '出现类似于如上代码那样的破坏性操作'，HotSpot还是'提供了一些参数' 作为 '元空间的防御措施'：
+
+        -XX:MetaspaceSize 指定元空间的'初始空间大小'，'以字节为单位'，'达到该值'就'会触发垃圾收集'进行'类型卸载'，'同时'收集器会'对该值进行调整'。
+        如果释放了大量的空间，就适当降低该值，如果释放了很少空间，则在不超过-XX:MaxMetaspaceSize（如果设置了的话）的情况下，适当提高该值
+
+        -XX:MaxMetaspaceSize '设置元空间最大值'，'默认'-1，即'不限制'，或者说'只受限于本地内存的大小'
+        -XX:MinMetaspaceFreeRatio 在GC后控制最小的元空间剩余容量的百分比，可'减小'因为元空间不足导致的'GC频率'
+        -XX:Max-MetaspaceFreeRatio 控制最大的元空间剩余容量的百分比
+
+
+    '本机直接内存溢出'
+        直接内存（Direct Memory）的'容量大小'可通过-XX:MaxDirectMemorySize指定，'若不指定'，则'默认与Java堆最大值'（-Xmx）一致。
+
+        这里越过DirectByteBuffer类，直接'通过反射获取Unsafe实例'进行'内存分配'。 
+        Unsafe类的getUnsafe()指定'只有引导类加载器'才会'返回实例'，体现了'设计者希望只有虚拟机标准类库里面的类才能使用Unsafe'，
+        'JDK10时' '才将Unsafe的部分功能' 通过VarHandle开放给外部。 
+        
+        虽然使用DirectByteBuffer分配内存'也会抛OOM'，但'它抛异常时' '并未真正向os申请分配内存'，
+        而是'通过计算得知内存无法分配'，就'在代码里手动抛了OOM'，真正申请分配内存的方法是'Unsafe::allocateMemory()'
+
+        使用unsafe分配本机内存
+            public class DirectMemoryOOM {
+                private static final int SIZE = 1024 * 1024
+                public static void main(String [〕 args) throws Exception {
+                    
+                    Field unsafeField = Unsafe.class.getDeclaredFields()[0];
+                    unsafeField. setAccessible(true);
+                    
+                    Unsafe unsafe = (Unsafe) unsafeField.get (null);
+                    while (true) {
+                        unsafe.allocateMemory(SIZE);
+                    }
+                }
+            }
+        结果
+
+        由'直接内存导致的内存溢出'，一个'明显的特征'是'在Heap Dump文件中不会看见有什么明显异常'，
+        若发现'内存溢出之后' '产生的Dump文件很小'，'而程序中又直接或间接使用了 DirectMemory'（比如使用'NIO'），则'该考虑直接内存'了。
